27cb4b0ca5e653a45cb238e51e414fa1cc4b46aa
Giri prasad <ainugiri@gmail.com>
2022-05-06 16:20:00 +0530

    202205061619

---
+++ src/test/java/AtosSpr/SpringEx01/SpringEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package AtosSpr.SpringEx01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEx01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
